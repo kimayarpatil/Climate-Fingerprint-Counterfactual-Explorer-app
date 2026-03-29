9950834d809da427f34e319d8078f2f93888e99d
KimayaPatil <kimayapatil553@gmail.com>
2026-03-29 13:14:47 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Climate AI Pro", layout="wide")
+
+st.title("🌍 Climate AI Pro")
+
+st.markdown("""
+### Welcome to AI-powered Climate Analysis App
+
+Use sidebar to explore:
+- 📊 EDA
+- 🖼️ Detection
+- 🎥 Webcam
+- 🔁 Simulation
+- 📈 Predictor
+""")
